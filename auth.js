72d7dbf86162c01528a2a9606d1df413b7a3f104
Adam <ligmaotoko@gmail.com>
2025-09-10 22:57:12 +0200

    FEAT: add authentication functionality

---
+++ auth.js
@@ -0,0 +1,26 @@
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+
+module.exports = function(passport, prisma) {
+    passport.use(new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
+        try {
+            const user = await prisma.user.findUnique({ where: { email } });
+            if (!user) return done(null, false, { message: 'Incorrect email or password.' });
+            const match = await bcrypt.compare(password, user.password);
+            if (!match) return done(null, false, { message: 'Incorrect email or password.' });
+            return done(null, user);
+        } catch (err) {
+            return done(err);
+        }    
+    }));
+
+    passport.serializeUser((user, done) => done(null, user.id));
+    passport.deserializeUser(async (id, done) => {
+        try {
+            const user = await prisma.user.findUnique({ where: { id }});
+            done(null, user || null);
+        } catch (err) {
+            done(err);
+        }
+    })
+}
