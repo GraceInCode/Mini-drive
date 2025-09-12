1e0e3263bbdb14aa496322eabe70d25f96f3e357
Adam <ligmaotoko@gmail.com>
2025-09-12 21:24:05 +0200

    Update the session model, and add expiresAt field to Session model
    and new migration for it

---
+++ prisma/migrations/20250912121253_add_expires_at/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `expires` on the `Session` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."Session" DROP COLUMN "expires",
+ADD COLUMN     "expiresAt" TIMESTAMP(3);
