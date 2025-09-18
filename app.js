b34b5313b4786583bad9fb59b1b99e902a3b264b
Adam <ligmaotoko@gmail.com>
2025-09-18 14:44:28 +0200

    Prepare for production: Secure session cookies, CORS, and start script

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ app.use(session({
     saveUninitialized: false,
     cookie: { 
       maxAge: 1000 * 60 * 60 * 24, // 1 day
-      secure: false, //
+      secure: process.env.NODE_ENV === 'production',
       sameSite: process.env.NODE_ENV === 'production' ? 'lax' : 'lax' 
     }, 
     store: new PrismaStore(prisma)
